dc4000b1d924cae32a8fa7a63fcf4b8be19ce381
jo ny <johnjony2525@gmail.com>
2026-08-17 11:46:18 +0530

    Implement basic mathematical operations in Python

---
+++ mathematical_operations.py
@@ -0,0 +1,15 @@
+# A program to perform mathematical operations between two user input values.
+a=int(input("Enter the first number: "))
+b=int(input("Enter the second number: "))
+
+add = a+b
+sub = a-b
+mult = a*b
+div = a/b
+mod = a%b
+
+print(f"The addition of the numbers is :",add)
+print(f"The subtraction of the numbers is :",sub)
+print(f"The multiplication of the numbers is :",mult)
+print(f"The division of the numbers is :",div)
+print(f"The modulus of the numbers is :",mod)
