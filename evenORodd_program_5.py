ea5de8ad6ae4118970616937d8e1b84a56836615
jo ny <johnjony2525@gmail.com>
2026-08-17 11:25:29 +0530

    Add files via upload

---
+++ evenORodd_program_5.py
@@ -0,0 +1,10 @@
+# Python program to check if a number is even or odd
+
+# Input from user
+number = int(input("Enter a number: "))
+
+# Check even or odd
+if number % 2 == 0:
+    print(number, "is an Even number.")
+else:
+    print(number, "is an Odd number.")
