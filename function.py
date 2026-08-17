5641b8c0f1c596435bb6f3aefd2f2325307345f7
jo ny <johnjony2525@gmail.com>
2026-08-17 11:47:39 +0530

    Add mathematical operations in function.py
    Implement mathematical operations: addition, subtraction, multiplication, division, and modulus.

---
+++ function.py
@@ -0,0 +1,15 @@
+# A program to perform mathematical operations between two values using python.
+
+def func(a,b):
+    add=a+b
+    print(f"The addition of the two numbers {a} and {b} are: ",add)
+    sub=a-b
+    print(f"The subtraction of the two numbers {a} and {b} are: ",sub)
+    mult=a*b
+    print(f"The multiplication of the two numbers {a} and {b} are: ",mult)
+    div=a/b
+    print(f"The division of the two numbers {a} and {b} are: ",div)
+    mod=a%b
+    print(f"The modulud of the two numbers {a} and {b} are: ",mod)
+    
+func(8,3)
