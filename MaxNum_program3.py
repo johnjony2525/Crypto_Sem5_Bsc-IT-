2662177a937e0fb95cb9389dabffa1756d58ee8c
jo ny <johnjony2525@gmail.com>
2026-08-17 11:23:46 +0530

    Add files via upload

---
+++ MaxNum_program3.py
@@ -0,0 +1,12 @@
+
+
+def MaxNum():
+    num1 = int(input("Enter a number:"))
+    num2 = int(input("Enter a number:"))
+
+    if(num1>num2):
+        print(f"Number {num1} is Maximum")
+    else:
+        print(f"Number {num2} is Maximum")
+MaxNum()
+
